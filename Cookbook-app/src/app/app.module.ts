ee2d0e36fe383fc7ed7a7b78d5710c9ca2ca9a37
Josvani-R <Josvani.l.Rivera@gmail.com>
2020-06-01 13:14:27 -0400

    login/index page

--- Cookbook-app/src/app/app.module.ts
+++ Cookbook-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatCardModule} from '@angular/material/card';
 import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -26,6 +28,7 @@ import { LoginFormComponent } from './components/login-form/login-form.component
     MatButtonModule,
     MatSelectModule,
     MatSnackBarModule,
+    MatCardModule,
     ReactiveFormsModule
 
 
