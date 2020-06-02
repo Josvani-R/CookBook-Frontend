5a20977a92c802b6770a911b9681b4676b699889
Josvani-R <Josvani.l.Rivera@gmail.com>
2020-06-02 16:46:06 -0400

    design update(not final)

--- Cookbook-app/src/app/app-routing.module.ts
+++ Cookbook-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 
-const routes: Routes = [{path: 'homepage', component: HomepageComponent}];
+const routes: Routes = [{path: 'homepage', component: HomepageComponent},
+{path:'', component:LoginFormComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
