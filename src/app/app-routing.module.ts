67c868b0da1bb0d1e6150d292984b6e000f82817
Philip <schupp@protonmail.com>
2020-06-05 15:33:16 -0400

    added profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
-
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import {RecipedetailComponent} from './components/recipedetail/recipedetail.component';
+import { RecipedetailComponent } from './components/recipedetail/recipedetail.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
-{path: 'homepage', component: HomepageComponent},
-{path: 'recipe/:id', component: RecipedetailComponent},
-{path:'', component:LoginFormComponent}
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'recipe/:id', component: RecipedetailComponent },
+  { path: '', component: LoginFormComponent },
+  { path: 'profile/:id', component: ProfileComponent },
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
